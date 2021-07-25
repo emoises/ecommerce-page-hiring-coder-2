24051104fb9c3ca06ad14e5a1f518300f5a7ad4c
emoises <eduardo.moises@gmail.com>
2021-07-25 20:41:13 -0300

    'Atualização para deplou no netlify'

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
+
+const SERVER_PORT = process.env.SERVER_PORT || 8888
+const SERVER_HOST = process.env.SERVER_HOST || 'http://localhost'
+
+console.log(SERVER_PORT)
+console.log(SERVER_HOST)
+
 const api = axios.create({
-    baseURL: 'http://localhost:8888/products'
+    baseURL: `${SERVER_HOST}:${SERVER_PORT}/products`
 })
 
 export default api;
